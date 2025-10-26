882b59d137b7c361844756c8e7294f291fe0a17c
Mark Sabotta <119641406+mark-sabotta@users.noreply.github.com>
2025-10-26 16:07:48 -0500

    Update index.js
    Added select options via js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 //form behavior
 //validation
+//generate select options
+//This list of 
+const Districts = [
+    "District A",
+    "District B",
+    "District C",
+    "District D",
+    "Other/Not Listed"
+];
+
+const selectDistrict = document.getElementById("school-district");
+
+Districts.forEach(districtName => {
+    const option = document.createElement("option");
+    option.value = districtName;
+    option.textContent = districtName;
+    selectElement.appendChild(option);
+});
